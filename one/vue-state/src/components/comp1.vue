f175a5b73dd464956fd2998fea54769cbc64d762
minchao920917 <499790879@qq.com>
2019-03-08 15:24:23 +0800

    state.js实现状态管理

---
+++ one/vue-state/src/components/comp1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="comp1">
+     <h1>Component 1</h1>
+    <input type="text" v-model="state.msg">
+    <!-- msg的双向绑定，绑定的是state管理下的msg -->
+  </div>
+</template>
+
+<script>
+import  state from "../state"
+
+export default {
+    name: 'comp1',
+    data(){
+      return{
+          state
+      }  
+    }
+}
+</script>
+
---
+++ one/vue-state/src/components/comp2.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="comp2">
+         <h1>Component 2</h1>
+        <input type="text" v-model="state.msg">
+        <!-- msg的双向绑定，绑定的是state管理下的msg -->
+    </div>
+</template>
+
+<script>
+import  state from "../state"
+
+export default {
+    name: 'comp2',
+    data(){
+        return{
+            state
+        }
+    }
+}
+</script>
